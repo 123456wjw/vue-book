b2c8e3adeb9e770a31b3506bbd088f00110c7bdd
wujiawei <18915788061@163.com>
2020-12-23 09:38:45 +0800

    新增获取typeList modules

--- src/store/modules/typeList/typeList.js
+++ src/store/modules/typeList/typeList.js
@@ -1,4 +1,7 @@
+import { doGet } from 'assets/js/http'
 import { SETTYPELIST } from './mutationTypes'
+import { GETTYPELIST } from './actionTypes'
+
 const state = {
 	typeList: []
 }
@@ -7,8 +10,22 @@ const mutations = {
 		state.typeList = typeList;
 	},
 }
+const actions = {
+	[GETTYPELIST]({commit}) {
+		return new Promise((resolve, reject) => {
+			doGet('getType').then(res => {
+				if (0 === res.code) {
+					commit(SETTYPELIST, {typeList: res.result})
+					resolve()
+				}
+			})
+		})
+		
+	}
+}
 export default {
 	namespaced: true, //用于在全局引用此文件里的方法时标识这一个的文件名
 	state,
 	mutations,
+	actions
 }
